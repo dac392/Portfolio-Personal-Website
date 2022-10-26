67f1f1af6793ffc3cdbba7d939cad5089bcd040e
dac392 <dac392@scarletmail.rutgers.edu>
2022-10-26 14:43:56 -0400

    I like this layout better

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import Home from './Components/Home';
-import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
         <Routes>
-          <Route exact path="/" element={ <Home /> } />
+          <Route exact path="/welcome" element={ <Home /> } />
+          <Route path="/resume" element={ <Home />} />
+          <Route path="/about" element={<Home/>} />
         </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
